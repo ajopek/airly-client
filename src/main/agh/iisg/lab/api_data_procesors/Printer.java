7ee361ca2aa268fc1494396ba4894074daf4cc98
ajopek <arjopek@gmail.com>
2018-01-22 01:46:32 +0100

    Add api data processors for printing data

--- src/main/agh/iisg/lab/api_data_procesors/Printer.java
+++ src/main/agh/iisg/lab/api_data_procesors/Printer.java
@@ -3,8 +3,8 @@ package agh.iisg.lab.api_data_procesors;
 import agh.iisg.lab.airly_api.AirlyData;
 
 public abstract class Printer implements IApiDataProcessor {
-    abstract String processData(AirlyData data);
-    abstract String getHeader();
+    abstract protected String processData(AirlyData data);
+    abstract protected String getHeader();
 
     public void apply(AirlyData data){
         System.out.println(getHeader());
